e6260762fc55e294d7ba06c5b8cbad2530c5bd99
ryoff <ryoffes@gmail.com>
2015-12-12 03:10:37 +0900

    add spec

--- spec/everything_becomes_f_spec.rb
+++ spec/everything_becomes_f_spec.rb
@@ -1,11 +1,58 @@
 require 'spec_helper'
+require 'pry'
 
-describe EverythingBecomesF do
+describe "#everything_became_f?" do
   it 'has a version number' do
     expect(EverythingBecomesF::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe Integer do
+    where(:integer, :result) do
+      [
+        [0, false],
+        [1, false],
+        [10, false],
+        [15, true],
+        [16, false],
+        [254, false],
+        [255, true],
+        [256, false],
+        [65534, false],
+        [65535, true],
+        [65536, false]
+      ]
+    end
+
+    with_them do
+      subject { integer.everything_became_f? }
+
+      it { is_expected.to eq result }
+    end
+  end
+
+  describe String do
+    where(:string, :result) do
+      [
+        ['a', false],
+        ['af', false],
+        ['fa', false],
+        ['A', false],
+        ['AF', false],
+        ['FA', false],
+        ['ffffffffffffffffffffffffffffffffffffffffffffffffffe', false],
+        ['effffffffffffffffffffffffffffffffffffffffffffffffff', false],
+        ['f', true],
+        ['fF', true],
+        ['FF', true],
+        ['ffffffffffffffffffffffffffffffffffffffffffffffffff', true],
+        ['FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', true]
+      ]
+    end
+
+    with_them do
+      subject { string.everything_became_f? }
+
+      it { is_expected.to eq result }
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'everything_becomes_f'
+require 'rspec-parameterized'
